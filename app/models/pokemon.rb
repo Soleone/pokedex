ebf2f4f9094cbc2282f3a8a8a8c98b7aa3d10d8f
Dennis Theisen <dennis@jadedpixel.com>
2010-05-20 05:01:43 +0200

    Fixed and renamed child association and allow selecting no parent in form.

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,4 +1,4 @@
 class Pokemon < ActiveRecord::Base
-  belongs_to :parent, :class_name => "Pokemon"
-  has_many :childs, :class_name => "Pokemon"
+  belongs_to :parent,     :class_name => "Pokemon"
+  has_many   :evolutions, :class_name => "Pokemon", :foreign_key => :parent_id
 end
